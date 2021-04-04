6f04cf8a5d54705120781a48a6ab73f908169134
PlayMa256 <matheus.g.silva@hotmail.com>
2021-04-04 13:04:15 -0300

    initial commit

---
+++ plopfile.js
@@ -0,0 +1,30 @@
+module.exports = function (plop) {
+  // create your generators here
+  plop.setGenerator("template", {
+    description: "template for a new package",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "package name",
+      },
+    ], // array of inquirer prompts
+    actions: [
+      {
+        type: "add",
+        path: "packages/{{dashCase name}}/package.json",
+        templateFile: "templates/package.json.hbs",
+			},
+      {
+        type: "add",
+        path: "packages/{{dashCase name}}/src/index.ts",
+        templateFile: "templates/index.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/{{dashCase name}}/tsconfig.json",
+        templateFile: "templates/tsconfig.json.hbs",
+      },
+    ],
+  });
+};
