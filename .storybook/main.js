787a93bf10d279c2c38b295dc1d16df83b5c0a38
Micaela Lechmann <micaela.lechmann@ifood.com.br>
2021-08-23 17:47:58 -0300

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../packages/**/*.stories.@(tsx|jsx)"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
+};
---
+++ packages/button/src/button.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default {
+  title: "button",
+};
+
+export const Accessible = () => <button>Accessible button</button>;
+
+export const Inaccessible = () => (
+  <button style={{ backgroundColor: "red", color: "darkRed" }}>
+    Inaccessible button
+  </button>
+);
